47777c2dc16d57260ba9233eafa80b325d0da253
kartikpandit987 <kartikpandit987@gmail.com>
2025-09-29 19:23:41 +0000

    commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import handler from './api/answer.js';
+
+dotenv.config({ path: '.env.local' });
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('AnswerAI server is running.');
+});
+
+app.post('/api/answer', (req, res) => {
+  handler(req, res);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
